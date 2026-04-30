6b4f1e18691ab9a89a1d0118d78330549bdef628
Dolly Diwakar <dollydiwakar3625@gmail.com>
2026-05-01 00:00:22 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Root from "./Root";
-import NewPage from "./components/NewPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Root />
-    <Routes>
-      <Route path="/new-page" element={<NewPage />} />
-    </Routes>
+
   </React.StrictMode>
 );
